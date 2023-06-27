c47eee2a09bf83a3f416a541e3288e975f97476d
rz <muhamaul@gmail.com>
2023-06-27 10:35:47 +0700

    finished chapter 20

---
+++ ch20-supervisor/band_supervisor.erl
@@ -0,0 +1,36 @@
+-module(band_supervisor).
+-behaviour(supervisor).
+
+-export([start_link/1]).
+-export([init/1]).
+
+start_link(Type) ->
+    supervisor:start_link({local, ?MODULE}, ?MODULE, Type).
+
+init(lenient) ->
+    init({one_for_one, 3, 60});
+init(angry) ->
+    init({rest_for_one, 2, 60});
+init(jerk) ->
+    init({one_for_all, 1, 60});
+init(jamband) ->
+    {ok, {{simple_one_for_one, 3, 60},
+         [{jam_musician,
+          {musicians, start_link, []},
+          temporary, 1000, worker, [musicians]}]}};
+init({RestartStrategy, MaxRestart, MaxTime}) ->
+    {ok, {{RestartStrategy, MaxRestart, MaxTime},
+         [{singer,
+          {musicians, start_link, [singer, good]},
+          permanent, 1000, worker, [musicians]},
+         {bass,
+         {musicians, start_link, [bass, good]},
+         temporary, 1000, worker, [musicians]},
+         {drum,
+         {musicians, start_link, [drum, bad]},
+         transient, 1000, worker, [musicians]},
+         {keytar,
+         {musicians, start_link, [keytar, good]},
+         transient, 1000, worker, [musicians]}]}}.
+
+
